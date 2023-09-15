36743317ebf2e0da3e014ca139718ce2fe0d3455
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-09-15 20:46:31 +0530

    Create Partition_Equal_Subset_Sum_Solution.cpp

---
+++ Partition_Equal_Subset_Sum_Solution.cpp
@@ -0,0 +1,89 @@
+***********************************************************************************************************************  Question  *************************************************************************************************
+
+Given an array arr[] of size N, check if it can be partitioned into two parts such that the sum of elements in both parts is the same.
+
+Example 1:
+
+Input: N = 4
+arr = {1, 5, 11, 5}
+Output: YES
+Explanation: 
+The two parts are {1, 5, 5} and {11}.
+Example 2:
+
+Input: N = 3
+arr = {1, 3, 5}
+Output: NO
+Explanation: This array can never be 
+partitioned into two such parts.
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function equalPartition() which takes the value N and the array as input parameters and returns 1 if the partition is possible. Otherwise, returns 0.
+
+Expected Time Complexity: O(N*sum of elements)
+Expected Auxiliary Space: O(N*sum of elements)
+
+Constraints:
+1 ≤ N ≤ 100
+1 ≤ arr[i] ≤ 1000
+N*sum of elements ≤ 5*106
+
+
+
+
+
+***********************************************************************************************************************  Solution  *************************************************************************************************
+
+  //{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int equalPartition(int n, int a[])
+    {
+      int sum = 0; 
+        for(int i = 0; i < n; i++)
+            sum += a[i];
+        if(sum % 2 != 0)
+            return 0;
+        
+        int dp[sum/2 + 1]; memset(dp, 0, sizeof(dp));
+        dp[0] = 1;
+            
+        for(int i = n - 1; i >= 0; i--){
+            for(int target = sum/2; target >= 0; target--){
+                if(target >= a[i])
+                    dp[target] |= dp[target - a[i]];
+            }
+        }
+        return dp[sum/2];
+        
+        }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        if(ob.equalPartition(N, arr))
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
